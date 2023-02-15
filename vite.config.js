75f861d90ab52ed2a1d2615dbce1b18ded60dc11
Marco Antonio Decormis Pinto <nesdufrik@gmail.com>
2023-02-15 18:55:52 -0400

    configuraciones basicas de vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import { BootstrapVue3Resolver } from 'unplugin-vue-components/resolvers'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    server: {
+        https: true,
+    },
+    plugins: [
+        mkcert(),
+        vue(),
+        Components({
+            resolvers: [BootstrapVue3Resolver],
+        }),
+    ],
+    history: {
+        fallback: true,
+    },
 })
