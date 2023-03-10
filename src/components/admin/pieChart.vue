38f162db1009a1cf743b86d59090c5a381b4819d
Marco Antonio Decormis Pinto <nesdufrik@gmail.com>
2023-03-10 17:40:58 -0400

    Update barChart and pieChart components with new data for the current month.

--- src/components/admin/pieChart.vue
+++ src/components/admin/pieChart.vue
@@ -2,7 +2,7 @@
     <div>
         <apexchart
             height="260"
-            type="pie"
+            type="donut"
             :options="options"
             :series="series"
         ></apexchart>
@@ -10,11 +10,58 @@
 </template>
 
 <script setup>
+const data = [
+    { fecha: '2023-03-22', item: 'Paceña 1lt', cantidad: 2 },
+    { fecha: '2023-03-22', item: 'Huari 1lt', cantidad: 4 },
+    { fecha: '2023-03-22', item: 'Potosina 1lt', cantidad: 5 },
+    { fecha: '2023-03-22', item: 'Paceña 1lt', cantidad: 3 },
+    { fecha: '2023-03-22', item: 'Paceña 2lt', cantidad: 3 },
+    { fecha: '2023-03-22', item: 'Paceña 3lt', cantidad: 3 },
+    { fecha: '2023-03-22', item: 'Paceña 4lt', cantidad: 3 },
+    { fecha: '2023-03-22', item: 'Paceña 4lt', cantidad: 3 },
+    { fecha: '2023-03-22', item: 'Paceña 4lt', cantidad: 3 },
+    { fecha: '2023-03-22', item: 'Paceña 4lt', cantidad: 3 },
+    { fecha: '2023-03-22', item: 'Paceña 5lt', cantidad: 3 },
+    { fecha: '2023-03-22', item: 'Paceña 5lt', cantidad: 3 },
+    { fecha: '2023-03-22', item: 'Paceña 6lt', cantidad: 3 },
+    { fecha: '2023-03-22', item: 'Paceña 6lt', cantidad: 3 },
+    { fecha: '2023-03-22', item: 'Paceña 7lt', cantidad: 3 },
+    { fecha: '2023-03-22', item: 'Paceña 7lt', cantidad: 3 },
+    { fecha: '2023-03-22', item: 'Wisky JW', cantidad: 3 },
+    { fecha: '2023-03-22', item: 'Jugo del Valle', cantidad: 2 },
+    { fecha: '2023-03-23', item: 'Paceña 1lt', cantidad: 12 },
+    { fecha: '2023-03-23', item: 'Huari 1lt', cantidad: 5 },
+    { fecha: '2023-03-23', item: 'Potosina 1lt', cantidad: 2 },
+    { fecha: '2023-03-23', item: 'Jugo del Valle', cantidad: 6 },
+    { fecha: '2023-03-24', item: 'Huari 1lt', cantidad: 1 },
+    { fecha: '2023-03-24', item: 'Potosina 1lt', cantidad: 8 },
+    { fecha: '2023-03-24', item: 'Paceña 1lt', cantidad: 2 },
+    { fecha: '2023-03-24', item: 'Jugo del Valle', cantidad: 4 },
+    { fecha: '2023-03-25', item: 'Huari 1lt', cantidad: 2 },
+    { fecha: '2023-03-25', item: 'Paceña 1lt', cantidad: 2 },
+    { fecha: '2023-03-26', item: 'Wisky JW', cantidad: 2 },
+    { fecha: '2023-03-27', item: 'Potosina 1lt', cantidad: 2 },
+]
+
+const filteredData = data.filter(d => {
+    const date = new Date(d.fecha)
+    return date.getMonth() === 2 && date.getFullYear() === 2023
+})
+
+let groupedData = {}
+filteredData.forEach(d => {
+    if (groupedData[d.item]) {
+        groupedData[d.item] += d.cantidad
+    } else {
+        groupedData[d.item] = d.cantidad
+    }
+})
+
 const options = {
     chart: {},
-    labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
+    labels: Object.keys(groupedData),
 }
-const series = [44, 55, 41, 17, 15]
+const series = Object.values(groupedData)
 </script>
 
 <style lang="scss" scoped></style>
