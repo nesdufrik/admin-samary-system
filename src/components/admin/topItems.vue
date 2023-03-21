2d6558ad6796d552864704478808849f1dcdb99e
Marco Antonio Decormis Pinto <nesdufrik@gmail.com>
2023-03-21 19:18:30 -0400

    Update topItems component to include icons and new post information.

--- src/components/admin/topItems.vue
+++ src/components/admin/topItems.vue
@@ -1,30 +1,46 @@
 <template>
     <div class="col">
         <div
-            class="rounded-2 bd-callout bd-callout-left bd-callout-secondary p-3 mt-0 mb-0"
+            class="rounded-2 bd-callout bd-callout-left bd-callout-secondary p-3 mt-0 mb-0 d-flex justify-content-between fw-bold"
         >
-            <h2 class="h1 fw-bold">Punto Nro 1</h2>
+            <h2 class="fs-1 material-icons-round align-self-center">edit</h2>
+            <div class="align-self-center text-center">
+                <h3 class="m-0">278</h3>
+                <small>New Posts</small>
+            </div>
         </div>
     </div>
     <div class="col">
         <div
-            class="rounded-2 bd-callout bd-callout-left bd-callout-secondary p-3 mt-0 mb-0"
+            class="rounded-2 bd-callout bd-callout-left bd-callout-secondary p-3 mt-0 mb-0 d-flex justify-content-between fw-bold"
         >
-            <h2 class="h1 fw-bold">Punto Nro 2</h2>
+            <h2 class="fs-1 material-icons-round align-self-center">edit</h2>
+            <div class="align-self-center text-center">
+                <h3 class="m-0">278</h3>
+                <small>New Posts</small>
+            </div>
         </div>
     </div>
     <div class="col">
         <div
-            class="rounded-2 bd-callout bd-callout-left bd-callout-secondary p-3 mt-0 mb-0"
+            class="rounded-2 bd-callout bd-callout-left bd-callout-secondary p-3 mt-0 mb-0 d-flex justify-content-between fw-bold"
         >
-            <h2 class="h1 fw-bold">Punto Nro 3</h2>
+            <h2 class="fs-1 material-icons-round align-self-center">edit</h2>
+            <div class="align-self-center text-center">
+                <h3 class="m-0">278</h3>
+                <small>New Posts</small>
+            </div>
         </div>
     </div>
     <div class="col">
         <div
-            class="rounded-2 bd-callout bd-callout-left bd-callout-secondary p-3 mt-0 mb-0"
+            class="rounded-2 bd-callout bd-callout-left bd-callout-secondary p-3 mt-0 mb-0 d-flex justify-content-between fw-bold"
         >
-            <h2 class="h1 fw-bold">Punto Nro 4</h2>
+            <h2 class="fs-1 material-icons-round align-self-center">edit</h2>
+            <div class="align-self-center text-center">
+                <h3 class="m-0">278</h3>
+                <small>New Posts</small>
+            </div>
         </div>
     </div>
 </template>
