731dd73d6af08221069db16fb8573a86c6113fe1
Marco Antonio Humberto Decormis Pinto <nesdufrik@gmail.com>
2023-08-12 17:06:18 -0400

    [UPDATE] login page add class to pointer

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,39 +1,37 @@
 <template>
-    <div
-        class="tarjeta rounded-2 bd-callout bd-callout-left bd-callout-secondary p-2 d-flex align-items-center"
-    >
-        <div>
-            <h2 class="fw-bold m-0 text-success ms-2">
-                {{ monthYear }}
-            </h2>
-        </div>
-        <div class="ms-auto">
-            <select
-                class="form-select border-0 fw-bold bg-light fs-5"
-                name=""
-                id=""
-                v-model="fechaFormateada"
-                @change="
-                    loadAnaliticsSpecific($route.params.id, fechaFormateada)
-                "
-            >
-                <option v-for="(el, index) in month" :value="index">
-                    {{ el }}
-                </option>
-            </select>
-        </div>
+  <div
+    class="tarjeta rounded-2 bd-callout bd-callout-left bd-callout-secondary p-2 d-flex align-items-center"
+  >
+    <div>
+      <h2 class="fw-bold m-0 text-success ms-2">
+        {{ monthYear }}
+      </h2>
     </div>
-    <div class="row row-cols-1 row-cols-md-2 g-3 mb-3">
-        <div class="col">
-            <barChart :chartTotalArray="chartTotales" />
-        </div>
-        <div class="col">
-            <pieChart :chartItemArray="chartItems" />
-        </div>
+    <div class="ms-auto">
+      <select
+        class="form-select border-0 fw-bold bg-light fs-5"
+        name=""
+        id=""
+        v-model="fechaFormateada"
+        @change="loadAnaliticsSpecific($route.params.id, fechaFormateada)"
+      >
+        <option v-for="(el, index) in month" :value="index">
+          {{ el }}
+        </option>
+      </select>
     </div>
-    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-3">
-        <topItems />
+  </div>
+  <div class="row row-cols-1 row-cols-md-2 g-3 mb-3">
+    <div class="col">
+      <barChart :chartTotalArray="chartTotales" />
     </div>
+    <div class="col">
+      <pieChart :chartItemArray="chartItems" />
+    </div>
+  </div>
+  <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-3">
+    <topItems />
+  </div>
 </template>
 
 <script setup>
@@ -45,26 +43,26 @@ import topItems from '../components/analiticas/topItems.vue'
 import { useAnalitics } from '../composables/useAnaliticas'
 
 const { loadAnalitics, loadAnaliticsSpecific, chartTotales, chartItems } =
-    useAnalitics()
+  useAnalitics()
 const route = useRoute()
 const month = [
-    'Enero',
-    'Febrero',
-    'Marzo',
-    'Abril',
-    'Mayo',
-    'Junio',
-    'Julio',
-    'Agosto',
-    'Septiempre',
-    'Octubre',
-    'Noviembre',
-    'Diciembre',
+  'Enero',
+  'Febrero',
+  'Marzo',
+  'Abril',
+  'Mayo',
+  'Junio',
+  'Julio',
+  'Agosto',
+  'Septiempre',
+  'Octubre',
+  'Noviembre',
+  'Diciembre',
 ]
 const actualDate = new Date()
 const fechaFormateada = ref(actualDate.getMonth())
 const monthYear = computed(() => {
-    return `${month[fechaFormateada.value]} ${actualDate.getFullYear()}`
+  return `${month[fechaFormateada.value]} ${actualDate.getFullYear()}`
 })
 
 loadAnalitics(route.params.id)
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,73 +1,68 @@
 <template>
-    <div class="d-flex flex-column text-center h-100">
-        <main class="form-signin w-100 m-auto flex-shrink-0">
-            <form @submit.prevent="login">
-                <img class="mb-4" src="../assets/login.png" alt="logo" />
-                <h1 class="h3 mb-3 fw-normal">Iniciar Sesión</h1>
-
-                <div class="form-floating">
-                    <input
-                        type="text"
-                        v-model="logInForm.email"
-                        class="form-control"
-                        id="Login"
-                    />
-                    <label for="Login">Usuario / Email</label>
-                </div>
-                <div class="form-floating">
-                    <input
-                        type="password"
-                        v-model="logInForm.password"
-                        class="form-control"
-                        id="Password"
-                    />
-                    <label for="Password">Contraseña</label>
-                </div>
-                <div class="form-check mb-3 text-start">
-                    <input
-                        class="form-check-input"
-                        type="checkbox"
-                        id="showPassword"
-                        v-model="showPassword"
-                        @click="toggleShowPassword"
-                    />
-                    <label
-                        class="form-check-label user-select-none"
-                        for="showPassword"
-                    >
-                        Mostrar contraseña
-                    </label>
-                </div>
-                <div
-                    class="alert alert-warning"
-                    role="alert"
-                    v-if="logInData.show"
-                >
-                    {{ logInData.message }}
-                </div>
-                <button
-                    class="w-100 btn btn-lg btn-primary"
-                    type="submit"
-                    v-if="!actionState"
-                >
-                    Iniciar Sesión
-                </button>
-                <button
-                    class="w-100 btn btn-lg btn-primary"
-                    type="button"
-                    v-else
-                    disabled
-                >
-                    <span
-                        class="spinner-border spinner-border-sm"
-                        role="status"
-                        aria-hidden="true"
-                    ></span>
-                    Iniciando...
-                </button>
-            </form>
-        </main>
-    </div>
+  <div class="d-flex flex-column text-center h-100">
+    <main class="form-signin w-100 m-auto flex-shrink-0">
+      <form @submit.prevent="login">
+        <img class="mb-4" src="../assets/login.png" alt="logo" />
+        <h1 class="h3 mb-3 fw-normal">Iniciar Sesión</h1>
+        <div class="form-floating">
+          <input
+            type="text"
+            v-model="logInForm.email"
+            class="form-control"
+            id="Login"
+          />
+          <label for="Login">Usuario / Email</label>
+        </div>
+        <div class="form-floating">
+          <input
+            type="password"
+            v-model="logInForm.password"
+            class="form-control"
+            id="Password"
+          />
+          <label for="Password">Contraseña</label>
+        </div>
+        <div class="form-check mb-3 text-start">
+          <input
+            class="form-check-input"
+            type="checkbox"
+            id="showPassword"
+            v-model="showPassword"
+            @click="toggleShowPassword"
+          />
+          <label
+            class="form-check-label user-select-none clase_label_check"
+            for="showPassword"
+          >
+            Mostrar contraseña
+          </label>
+        </div>
+        <div class="alert alert-warning" role="alert" v-if="logInData.show">
+          {{ logInData.message }}
+        </div>
+        <button
+          class="w-100 btn btn-lg btn-primary"
+          type="submit"
+          v-if="!actionState"
+        >
+          Iniciar Sesión
+        </button>
+        <button
+          class="w-100 btn btn-lg btn-primary"
+          type="button"
+          v-else
+          disabled
+        >
+          <span
+            class="spinner-border spinner-border-sm"
+            role="status"
+            aria-hidden="true"
+          ></span>
+          Iniciando...
+        </button>
+      </form>
+    </main>
+  </div>
 </template>
 
 <script setup>
@@ -79,43 +74,47 @@ const { logInForm, logInData, actionState, login } = useAuth()
 const route = useRoute()
 
 if (route.query.show && route.query.message) {
-    logInData.value = {
-        show: route.query.show,
-        message: route.query.message,
-    }
+  logInData.value = {
+    show: route.query.show,
+    message: route.query.message,
+  }
 }
 
 let showPassword = false
 
 function toggleShowPassword() {
-    const passwordInput = document.getElementById('Password')
-    if (passwordInput.type === 'password') {
-        passwordInput.type = 'text'
-    } else {
-        passwordInput.type = 'password'
-    }
+  const passwordInput = document.getElementById('Password')
+  if (passwordInput.type === 'password') {
+    passwordInput.type = 'text'
+  } else {
+    passwordInput.type = 'password'
+  }
 }
 </script>
 
 <style scoped>
 .form-signin {
-    max-width: 330px;
-    padding: 15px;
+  max-width: 330px;
+  padding: 15px;
 }
 
 .form-signin .form-floating:focus-within {
-    z-index: 2;
+  z-index: 2;
 }
 
 .form-signin #Login {
-    margin-bottom: -1px;
-    border-bottom-right-radius: 0;
-    border-bottom-left-radius: 0;
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
 }
 
 .form-signin #Password {
-    margin-bottom: 10px;
-    border-top-left-radius: 0;
-    border-top-right-radius: 0;
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+
+.clase_label_check {
+  cursor: pointer;
 }
 </style>
